11431b8f5e0b465d097178284de473c5bd39c86e
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-08-28 06:08:21 +0800

    Build user management entrypoint

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            input: ['resources/css/app.css', 'resources/js/app.tsx', 'resources/js/users.tsx'],
             refresh: true,
         }),
         react(),
