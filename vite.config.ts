671fbfc567c13c47328bf3bc80a59e663dc3b6a9
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-08-28 23:29:27 +0800

    Build student material checklist bundle

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
                 'resources/js/users.tsx',
                 'resources/js/classroom.tsx',
                 'resources/js/student-progress.tsx',
+                'resources/js/student-material-checklist.tsx',
             ],
             refresh: true,
         }),
