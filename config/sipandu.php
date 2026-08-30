1d62e7686b128fba7d624ba91ece6a4f3dd92adc
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-08-30 19:09:14 +0800

    feat: tambah konfigurasi base path SiPANDU

---
+++ config/sipandu.php
@@ -0,0 +1,15 @@
+<?php
+
+$appUrl = (string) env('APP_URL', '');
+$derivedPath = (string) (parse_url($appUrl, PHP_URL_PATH) ?: '');
+$configuredPath = trim((string) env('APP_BASE_PATH', $derivedPath));
+
+if ($configuredPath === '' || $configuredPath === '/') {
+    $basePath = '';
+} else {
+    $basePath = '/'.trim($configuredPath, '/');
+}
+
+return [
+    'base_path' => $basePath,
+];
