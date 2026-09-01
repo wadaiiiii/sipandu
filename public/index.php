fbc01b50afdb9e581849bbcfc6c0cbd598e38b47
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-09-01 08:03:46 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,13 +5,15 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+$appPath = '/home/matematikaunsulb/apps/sipandu/current';
+
+if (file_exists($maintenance = $appPath.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-require __DIR__.'/../vendor/autoload.php';
+require $appPath.'/vendor/autoload.php';
 
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once $appPath.'/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
