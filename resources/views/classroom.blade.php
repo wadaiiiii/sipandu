c0f175ff77c5bda356b96ffd4f0fb83255114c74
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-09-08 00:38:28 +0800

    Load icon-only delete controls in classroom

--- resources/views/classroom.blade.php
+++ resources/views/classroom.blade.php
@@ -13,7 +13,7 @@
         <script>
         (() => {
             const base = String(window.__SIPANDU_BASE_PATH__ || '').replace(/\/+$/, '');
-            ["sipandu-classroom-reliability.js", "sipandu-classroom-actions.js"].forEach((name) => {
+            ["sipandu-classroom-reliability.js", "sipandu-classroom-actions.js", "sipandu-delete-icons.js"].forEach((name) => {
                 const script = document.createElement('script');
                 script.src = (base ? base : '') + '/' + name + '?v=20260908.1';
                 script.defer = true;
